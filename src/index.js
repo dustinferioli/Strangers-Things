68f053cb66402332e9212852fb7442a569d472b5
dustinferioli <dustin.ferioli@icloud.com>
2022-07-05 16:20:50 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react";
+
+// import { } from './components';
+
+const App = () => {
+
+
+
+    return <div className="app">
+        {/* components go here */}
+    </div>
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
